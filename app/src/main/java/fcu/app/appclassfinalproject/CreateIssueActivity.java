25d96b9dcb73fcdd3298ad330c3568b7ab89eb3d
Haspeare <haoxuan1117@gmail.com>
2025-05-12 16:09:19 +0800

    refactor : update activity_create_issue

--- app/src/main/java/fcu/app/appclassfinalproject/CreateIssueActivity.java
+++ app/src/main/java/fcu/app/appclassfinalproject/CreateIssueActivity.java
@@ -1,13 +1,30 @@
 package fcu.app.appclassfinalproject;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateIssueActivity extends AppCompatActivity {
     private EditText etPurpose;
@@ -16,6 +33,11 @@ public class CreateIssueActivity extends AppCompatActivity {
     private EditText etEndTime;
     private EditText etStatus;
     private EditText etDesignee;
+    private ImageButton btnHome;
+    private ImageButton btnSave;
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +50,9 @@ public class CreateIssueActivity extends AppCompatActivity {
             return insets;
         });
 
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+
         //找對應id
         etPurpose = findViewById(R.id.et_purpose);
         etOverview = findViewById(R.id.et_overview);
@@ -36,5 +61,66 @@ public class CreateIssueActivity extends AppCompatActivity {
         etStatus = findViewById(R.id.et_status);
         etDesignee = findViewById(R.id.et_designee);
 
+        btnHome = findViewById(R.id.btn_home);
+        btnSave = findViewById(R.id.btn_save);
+
+//
+//        SharedPreferences prefs = getSharedPreferences("", MODE_PRIVATE);
+//        String projectId = prefs.getString("projectId",null);
+
+        // 假設 sharedPreference 有　"projectId"
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateIssueActivity.this, ProjectActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 假設 sharedPreference 有　"projectId"
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name = etPurpose.getText().toString().trim();
+                String summary = etOverview.getText().toString().trim();
+                String start_time = etStartTime.getText().toString().trim();
+                String end_time = etEndTime.getText().toString().trim();
+//                String status = etStatus.getText().toString().trim();
+//                String designee = etDesignee.getText().toString().trim();
+
+
+                Map<String, Object> issue = new HashMap<>();
+//                issue.put("id",id);
+                issue.put("name",name);
+                issue.put("summary",summary);
+                issue.put("start_time",start_time);
+                issue.put("end_time",end_time);
+//                issue.put("project_id",projectId);
+//                issue.put("status",status);
+//                issue.put("designee",designee);
+                db.collection("issues").add(issue).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(CreateIssueActivity.this, "建立成功，ID:", Toast.LENGTH_LONG).show();
+                        clearFields();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(CreateIssueActivity.this, "建立失敗，請檢查輸入是否正確", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
+            }
+        });
     }
+    private void clearFields() {
+        etPurpose.setText("");
+        etOverview.setText("");
+        etStartTime.setText("");
+        etEndTime.setText("");
+    }
+
 }
